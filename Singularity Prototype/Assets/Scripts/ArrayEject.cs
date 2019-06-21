9db491100fc2c30ef87b61268c3da7068b68ad8a
chris8736 <chris8736@github.com>
2019-06-21 11:51:48 -0400

    added skeleton project
    variable and array prefabs

---
+++ Singularity Prototype/Assets/Scripts/ArrayEject.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ArrayEject : MonoBehaviour
+{
+    Vector2 startPosition = Vector2.zero;
+    Vector2 targetPosition = new Vector2(2.3f, 0);
+    Vector2 velocity = Vector2.zero;
+    public float smoothTime = .3f;
+    int data;
+    bool ejected, ejecting = false;
+    public GameObject page;
+    public GameObject arrayHolder;
+    public GameObject arrayDataText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        arrayDataText.GetComponent<MeshRenderer>().sortingOrder = -1;
+    }
+
+    void Update()
+    {
+        if (ejecting){
+            if (!ejected)
+            {
+                if (page.transform.localPosition.x < targetPosition.x - .01)
+                {
+                    page.transform.localPosition = Vector2.SmoothDamp(page.transform.localPosition, targetPosition, ref velocity, smoothTime);
+                }
+                else
+                {
+                    ejected = true;
+                    ejecting = false;
+                }
+            }
+            else
+            {
+                if (page.transform.localPosition.x > startPosition.x + .01)
+                {
+                    page.transform.localPosition = Vector2.SmoothDamp(page.transform.localPosition, startPosition, ref velocity, smoothTime);
+                }
+                else
+                {
+                    ejected = false;
+                    ejecting = false;
+                }
+            }
+        }
+    }
+
+    public void Eject()
+    {
+        if (!ejecting)
+        {
+            transform.Rotate(0, 0, 180);
+            if (!ejected)
+            {
+                data = arrayHolder.GetComponent<ArrayIndex>().getData();
+                arrayDataText.GetComponent<TextMeshPro>().text = data.ToString();
+            }
+            ejecting = true;
+        }
+    }
+}
---
+++ Singularity Prototype/Assets/Scripts/ArrayIndex.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ArrayIndex : MonoBehaviour
+{
+    int index = 0;
+    TextMeshProUGUI tm;
+    List<int> data;
+
+    void Start()
+    {
+        tm = GetComponent<TextMeshProUGUI>();
+        tm.text = index.ToString();
+        data = new List<int>();
+        for (int i = 0; i < 100; i++)
+        {
+            data.Add(i * i);
+        }
+    }
+
+    public void changeText(int i)
+    {
+        Debug.Log(index);
+        index = Mathf.Clamp(index + i, 0, data.Count - 1);
+        tm.text = index.ToString();
+    }
+
+    public int getData()
+    {
+        return data[index];
+    }
+}
